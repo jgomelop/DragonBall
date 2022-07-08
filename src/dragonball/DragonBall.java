29dcbd6c661a44c21a5975e4fb78ae64c902d57a
unknown <jpgl2498@gmail.com>
2022-07-08 17:22:10 -0500

    Se construye clase GameMap que contiene la información del mapa del juego. Se eliminan líneas del main y se pasan a GameMap

--- src/dragonball/DragonBall.java
+++ src/dragonball/DragonBall.java
@@ -1,31 +1,14 @@
 package dragonball;
 import dao.LinkedList;
+import models.GameMap;
 /**
  *
  * @author Juan Pablo Gómez López
  */
 public class DragonBall {
-    public static int generateValue(float prob) {
-        float random_int = (float) Math.random();
-        if (random_int <= prob) {
-            return 1;
-        }
-        return 0;
-    }
+    
 
-    public static String[][] intMapToStringMap(int[][] map) {
-        String[][] stringMap = new String[map.length][map[0].length];
-
-        for (int i = 0; i <= map.length - 1; i++) {
-            for (int j = 0; j <= map[0].length - 1; j++) {
-                stringMap[i][j] = String.valueOf(map[i][j]);
-//                System.out.print(stringMap[i][j] + " ");
-            }
-//            System.out.println();
-
-        }
-        return stringMap;
-    }
+   
     /**
      * @param args the command line arguments
      */
@@ -33,7 +16,7 @@ public class DragonBall {
 
         LinkedList registro = new LinkedList();
         registro.add("HP 100");
-        registro.add("Recivió 2 ataques");
+        registro.add("Recibió 2 ataques");
         registro.add("Realizó 5 movimientos");
         String historial = registro.output();
 
@@ -42,29 +25,9 @@ public class DragonBall {
             historial = registro.output();
         }
 
-        int[][] map = new int[5][5];
-        for (int i = 0; i <= 4; i++) {
-            for (int j = 0; j <= 4; j++) {
-                if ((i == 0 && j == 0) || (i == 4 && j == 4)) {
-                    map[i][j] = 0;
-                } else {
-                    map[i][j] = generateValue((float) 0.65);
-                }
-                /*System.out.print(map[i][j] + " ");*/
-            }
-            /*System.out.println();*/
-        }
         
-        /* # Representa la posicion del jugador*/
-        String[][] mapString = intMapToStringMap(map);
-        mapString[0][0] = "#";
-        
-        for (int i = 0; i <= mapString.length - 1; i++) {
-            for (int j = 0; j <= mapString[0].length - 1; j++) {
-                System.out.print(mapString[i][j] + " ");
-            }
-            System.out.println();
-
-        }
+        GameMap mapa = new GameMap();
+        mapa.printMap();
+       
     }
 }
--- src/models/GameMap.java
+++ src/models/GameMap.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 /**
@@ -10,5 +5,73 @@ package models;
  * @author Juan Pablo
  */
 public class GameMap {
+
+    private String[][] matrix = new String[5][5];
+    private Position posJugador = new Position(0,0);
+
+    public GameMap() {
+        
+        /**
+         * Rellena la matriz matrix con "0" si 
+         */
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <= 4; j++) {
+                if (i == 0 && j == 0) { // Posición inical del jugador
+                    this.matrix[i][j] = "#";
+                } else if (i == 4 && j == 4){ //Pos. Final. Meta del juego
+                    this.matrix[i][j] = "-";
+                }
+                else {
+                    // Genera valores "0" o "1" dependiendo de probabilidad
+                    // en este caso, 65%
+                    this.matrix[i][j] = generateValue((float) 0.65);
+                }
+            }
+        }
+    }
+
+    /**
+     * Retorna "0" o "1" dependiendo del valor de probabilidad prob
+     * de acuerdo a la función de probabilidad uniforme de Math.random
+     * @param prob float. Probabilidad. Valor entre 0 y 1.
+     * @return  "1" si random_int es menor o igual a prob, "0" en lo contrario.
+     */
+    private String generateValue(float prob) {
+        float random_int = (float) Math.random();
+        if (random_int <= prob) {
+            return String.valueOf(1);
+        }
+        return String.valueOf(0);
+    }
+    
+    /* Imprime el matrixa en pantalla */
+    public void printMap() {
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <= 4; j++) {
+                 System.out.print(this.matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    /**
+     * 
+     * @param i Valor de la i-ésima fila a consultar
+     * @param j Valor de la j-ésima columna a consultar
+     * @return Valor String de la posición (i,j) de la matriz
+     */
+    public String getValue(int i, int j){
+        return this.matrix[i][j];
+    }
+    
+    /**
+     * 
+     * @param s String. Valor a insertar en la matriz.
+     * @param i Valor de la i-ésima fila a insertar.
+     * @param j Valor de la j-ésima columna a insertar.
+     */
+    public void setValue(String s, int i, int j){
+        this.matrix[i][j] = s;
+    }
     
 }
