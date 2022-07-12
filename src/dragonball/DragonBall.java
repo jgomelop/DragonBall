074eac67740ad0aca3c541a3c75bbc0f3f136f44
MainTopita <carolina.melguizop@udea.edu.co>
2022-07-11 21:29:19 -0500

    mensaje

---
+++ src/dragonball/DragonBall.java
@@ -0,0 +1,60 @@
+package dragonball;
+
+/**
+ *
+ * @author Juan Pablo Gómez López
+ */
+public class DragonBall {
+
+    public static int generateValue(float prob) {
+        float random_int = (float) Math.random();
+        if (random_int <= prob) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static String[][] intMapToStringMap(int[][] map) {
+        String[][] stringMap = new String[map.length][map[0].length];
+
+        for (int i = 0; i <= map.length - 1; i++) {
+            for (int j = 0; j <= map[0].length - 1; j++) {
+                stringMap[i][j] = String.valueOf(map[i][j]);
+//                System.out.print(stringMap[i][j] + " ");
+            }
+//            System.out.println();
+
+        }
+        return stringMap;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[][] map = new int[5][5];
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 0; j <= 4; j++) {
+                if ((i == 0 && j == 0) || (i == 4 && j == 4)) {
+                    map[i][j] = 0;
+                } else {
+                    map[i][j] = generateValue((float) 0.65);
+                }
+                /*System.out.print(map[i][j] + " ");*/
+            }
+            /*System.out.println();*/
+        }
+        
+        /* # Representa la posicion del jugador*/
+        String[][] mapString = intMapToStringMap(map);
+        mapString[0][0] = "#";
+        
+        for (int i = 0; i <= mapString.length - 1; i++) {
+            for (int j = 0; j <= mapString[0].length - 1; j++) {
+                System.out.print(mapString[i][j] + " ");
+            }
+            System.out.println();
+
+        }
+    }
+}
---
+++ src/models/Character.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Juan Pablo
+ */
+public class Character {
+    private String name;
+    private int hp;
+    private int atk;
+    private int def;
+
+    public Character(String name, int hp, int atk, int def) {
+        this.name = name;
+        this.hp = hp;
+        this.atk = atk;
+        this.def = def;
+    }
+
+    public Character() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getAtk() {
+        return atk;
+    }
+
+    public void setAtk(int atk) {
+        this.atk = atk;
+    }
+
+    public int getDef() {
+        return def;
+    }
+
+    public void setDef(int def) {
+        this.def = def;
+    }
+
+    @Override
+    public String toString() {
+        return "Character{" + "name=" + name + ", hp=" + hp + ", atk=" + atk + ", def=" + def + '}';
+    }
+    
+    
+ public void curar() {
+      int vidaRegenerada = (int)(Math.random()*10+1);
+      this.hp += vidaRegenerada;
+      System.out.println("El personaje se ha curado" + vidaRegenerada + ",su nueva vida es:" + this.hp);
+     }
+}
