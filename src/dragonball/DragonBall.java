09afd8fc2c1ed76948d187ba35a390e2dd3d0b97
Juan Pablo Gómez López <Juan Pablo Gmez Lpez>
2022-08-02 15:31:54 -0500

    Se implementa menú de selección de personaje

--- src/dragonball/DragonBall.java
+++ src/dragonball/DragonBall.java
@@ -15,6 +15,30 @@ import models.Position;
  */
 public class DragonBall {
 
+    public static String playerSelectionMenu() {
+        String seleccion;
+        String msg;
+        String separador = new String(new char[20]).replace("\0", "-");
+        Scanner entradaUsuario = new Scanner(System.in);
+
+        msg = String.join(System.getProperty("line.separator"),
+                separador,
+                "SELECCION DE PERSONAJE",
+                "Ingrese el valor numérico correspondiente. ",
+                "1: Gokú. HP: 120, ATK: 22, DEF: 10",
+                "2: Vegetta. HP: 150, ATK: 15, DEF: 20",
+                "3: Kirllin. HP: 90, ATK: 8, DEF: 8",
+                separador
+        );
+
+        System.out.println(msg);
+
+        seleccion = entradaUsuario.next();
+
+        //entradaUsuario.close();
+        return seleccion;
+    }
+    
     public static String movementMenu() {
         String seleccion;
         String msg;
@@ -99,29 +123,29 @@ public class DragonBall {
     public static String eventHandler(int value, Player player, Enemy enemy) {
         String msg;
         if (value == 1) {
-            int eventSw = (int) Math.round(Math.random()*101); // random [0,100)
+            int eventSw = (int) Math.round(Math.random() * 101); // random [0,100)
             if (eventSw <= 25) {
-                
+
                 int healedValue = (player.getHp() == player.getMaxHealth()) ? 0 : player.heal();
-                
-                msg = player.getName() + " recupera " 
-                      + String.valueOf(healedValue) 
-                      + " HP";
+
+                msg = player.getName() + " recupera "
+                        + String.valueOf(healedValue)
+                        + " HP";
                 return msg;
-                
+
             } else if (eventSw > 25 && eventSw <= 65) {
                 int enemyDmgValue = enemy.attack(player);
-                
-                msg = player.getName() + " pierde " 
-                      + String.valueOf(enemyDmgValue) 
-                      + " HP";
+
+                msg = player.getName() + " pierde "
+                        + String.valueOf(enemyDmgValue)
+                        + " HP";
                 return msg;
-                
+
             } else {
                 int playerDmgValue = player.attack(enemy);
-                 msg = enemy.getName() + " pierde " 
-                      + String.valueOf(playerDmgValue) 
-                      + " HP";
+                msg = enemy.getName() + " pierde "
+                        + String.valueOf(playerDmgValue)
+                        + " HP";
                 return msg;
             }
         } else {
@@ -130,14 +154,39 @@ public class DragonBall {
         }
     }
 
+             
     public static void main(String[] args) {
 
-        //creacion de personajes 
+        
         Player goku = new Player("Goku", 120, 22, 10);
         Player vegetta = new Player("Vegetta", 150, 15, 20);
         Player kirllin = new Player("Kirllin", 90, 8, 8);
         Enemy freezer = new Enemy("Freezer", 300, 5);
-
+        
+        Player player;
+        
+        // Selección de personaje 
+        int count = 0; // contador para máximo de intentos
+        while (true) {
+            String playerSelection = playerSelectionMenu();
+            
+            if ( 1 == Integer.valueOf(playerSelection) || count >= 10 ){
+                player = goku;
+                break;
+            } 
+            else if ( 2 == Integer.valueOf(playerSelection) ){
+                player = vegetta;
+                break;
+            }
+            else if ( 3 == Integer.valueOf(playerSelection) ){
+                player = kirllin;
+                break;
+            }
+            
+            count += 1;
+            System.out.println("Parámentro no válido.");
+        }
+        
         GameMap map = new GameMap();
         map.printPlayerMap();
 
@@ -151,9 +200,8 @@ public class DragonBall {
                 map.printPlayerMap();
             } else {
                 int eventValue = movementHandler(sel, map);
-                String eventMsg = eventHandler(eventValue, goku, freezer);
+                String eventMsg = eventHandler(eventValue, player, freezer);
                 System.out.println(eventMsg);
-
             }
         }
 
