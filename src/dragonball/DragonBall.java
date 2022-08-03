9db66ef4bc14ea0983444a0c6c427f8a569b0681
Juan Pablo Gómez López <Juan Pablo Gmez Lpez>
2022-08-03 11:35:01 -0500

    se implementa el sistema de puntajes al juego

--- src/dragonball/DragonBall.java
+++ src/dragonball/DragonBall.java
@@ -7,6 +7,7 @@ import models.Enemy;
 import java.util.Scanner;
 import models.GameMap;
 import models.Position;
+import models.Event;
 
 /**
  *
@@ -38,7 +39,7 @@ public class DragonBall {
         //entradaUsuario.close();
         return seleccion;
     }
-    
+
     public static String movementMenu() {
         String seleccion;
         String msg;
@@ -113,20 +114,19 @@ public class DragonBall {
         String new_i = String.valueOf(pos.getI());
         String new_j = String.valueOf(pos.getJ());
 
-        mapa.printPlayerMap();
-        
+        //mapa.printPlayerMap();
         String msg = String.format("(%s,%s) -> (%s,%s)", old_i, old_j, new_i, new_j);
         System.out.println(msg);
-        
-        
 
         return mapa.getEventValueInPos(pos);
     }
 
-    public static String eventHandler(int value, Player player, Enemy enemy) {
+    public static Event eventHandler(int value, Player player, Enemy enemy) {
         String msg;
+        Event event = new Event();
         if (value == 1) {
             int eventSw = (int) Math.round(Math.random() * 101); // random [0,100)
+
             if (eventSw <= 25) {
 
                 int healedValue = (player.getHp() == player.getMaxHealth()) ? 0 : player.heal();
@@ -134,7 +134,10 @@ public class DragonBall {
                 msg = player.getName() + " recupera "
                         + String.valueOf(healedValue)
                         + " HP";
-                return msg;
+
+                event.setMsg(msg);
+                event.setEarnedPoints(0);
+                return event;
 
             } else if (eventSw > 25 && eventSw <= 65) {
                 int enemyDmgValue = enemy.attack(player);
@@ -142,46 +145,47 @@ public class DragonBall {
                 msg = player.getName() + " pierde "
                         + String.valueOf(enemyDmgValue)
                         + " HP";
-                return msg;
+
+                event.setMsg(msg);
+                event.setEarnedPoints(0);
+                return event;
 
             } else {
                 int playerDmgValue = player.attack(enemy);
                 msg = enemy.getName() + " pierde "
                         + String.valueOf(playerDmgValue)
                         + " HP";
-                return msg;
+                event.setMsg(msg);
+                event.setEarnedPoints(playerDmgValue);
+                return event;
             }
         } else {
             msg = "No sucede nada.";
-            return msg;
+            event.setMsg(msg);
+            event.setEarnedPoints(0);
+            return event;
         }
     }
 
-             
     public static void main(String[] args) {
 
-        
         Player goku = new Player("Goku", 120, 22, 10);
         Player vegetta = new Player("Vegetta", 150, 15, 20);
         Player kirllin = new Player("Kirllin", 90, 8, 8);
         Enemy enemy = new Enemy("Freezer", 300, 5);
-        
         Player player;
-        
         // Selección de personaje 
         int count = 0; // contador para máximo de intentos
         while (true) {
             String playerSelection = playerSelectionMenu();
-            
-            if ( 1 == Integer.valueOf(playerSelection) || count >= 10 ){
+
+            if (1 == Integer.valueOf(playerSelection) || count >= 10) {
                 player = goku;
                 break;
-            } 
-            else if ( 2 == Integer.valueOf(playerSelection) ){
+            } else if (2 == Integer.valueOf(playerSelection)) {
                 player = vegetta;
                 break;
-            }
-            else if ( 3 == Integer.valueOf(playerSelection) ){
+            } else if (3 == Integer.valueOf(playerSelection)) {
                 player = kirllin;
                 break;
             }
@@ -189,7 +193,9 @@ public class DragonBall {
             count += 1;
             System.out.println("Parámentro no válido.");
         }
-        
+
+        int gamePoints = 0;
+
         GameMap map = new GameMap();
         map.printPlayerMap();
 
@@ -204,26 +210,33 @@ public class DragonBall {
                 map.printPlayerMap();
             } else {
                 int eventValue = movementHandler(sel, map);
-                String eventMsg = eventHandler(eventValue, player, enemy);
+                Event event = eventHandler(eventValue, player, enemy);
+                String eventMsg = event.getMsg();
+                int eventEarnedPoints = event.getEarnedPoints();
+                gamePoints += eventEarnedPoints;
+
                 System.out.println(eventMsg);
-                
-                if (player.getHp() <= 0){
+                System.out.printf("PV de %s: %d\n", player.getName(), player.getHp());
+                System.out.printf("PV de %s: %d\n", enemy.getName(), enemy.getHp());
+                System.out.printf("Puntos: %d\n", gamePoints);
+
+                if (player.getHp() <= 0) {
                     System.out.println("Has perdido!");
-                    System.out.printf("ültima Posición: (%d,%d)", 
-                                      map.getplayerPos().getI(),
-                                      map.getplayerPos().getJ());
-                    
+                    System.out.printf("ültima Posición: (%d,%d)",
+                            map.getplayerPos().getI(),
+                            map.getplayerPos().getJ());
+
                     break;
-                }
-                else if (enemy.getHp() <= 0 
-                        || (map.getplayerPos().getI() == 4 
-                            && map.getplayerPos().getJ() == 4)) {
-                    
+                } else if (enemy.getHp() <= 0
+                        || (map.getplayerPos().getI() == 4
+                        && map.getplayerPos().getJ() == 4)) {
+
                     System.out.println("Has Ganado!");
                     break;
                 }
+
             }
-            
+
         }
 
     }
